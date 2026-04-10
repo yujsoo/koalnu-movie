9dc164b29faaf175f38b7200d503cc9ccf092215
유지수 <jsyu333@gmail.com>
2026-04-10 21:44:20 +0900

    feat: 라우트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css'
-import {Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import AppLayout from "./layout/AppLayout.jsx";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage.jsx";
 
 // 홈페이지
 // 영화 전체 보여주는 페이지
 // 영화 디테일 페이지
+
 function App() {
 
   return (
     <>
       <Routes>
         <Route path="/" element={<AppLayout/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path='movies'>
+            <Route index element={<MoviePage/>}/>
+            <Route path=":id" element={<MovieDetailPage/>}/>
+          </Route>
 
+          {/* 일반적인 경우 */}
+          {/*<Route path="/movies" element={<MoviePage/>}/>*/}
+          {/*<Route path="/movies/:id" element={<MovieDetailPage/>}/>*/}
         </Route>
       </Routes>
     </>
