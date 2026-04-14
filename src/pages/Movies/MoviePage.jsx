6ad436df2fc8cf98330f3e4c61b89bbbac303d7c
유지수 <jsyu333@gmail.com>
2026-04-15 00:02:07 +0900

    feat: pagination 스타일 추가

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -2,10 +2,17 @@ import {useSearchMovie} from "../../hooks/useSearchMovie.js";
 import {useSearchParams} from "react-router-dom";
 import MovieCard from "../../common/MovieCard/MovieCard.jsx";
 import './MoviPage.style.css'
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import ReactPaginateImport from "react-paginate";
+
 
 const MoviePage = () => {
   const [searchParams] = useSearchParams();
   const searchKeyword = searchParams.get("keyword");
+  const ReactPaginate =
+      ReactPaginateImport?.default ?? ReactPaginateImport;
+
   // 경로 두가지
   // nav바에서 클릭해서 온 경우
   // keyword를 입력해서 온 경우 -> Keyword와 관련된 영화를 보여줌
@@ -27,6 +34,10 @@ const MoviePage = () => {
   };
   const filteredMovies = filterMovies();
 
+  const handlePageClick = () => {
+
+  }
+
   if (!filteredMovies || filteredMovies.length === 0) {
     return <div className={'guide'}>검색 결과가 없습니다.</div>
   }
@@ -36,9 +47,27 @@ const MoviePage = () => {
         <ul className={'movies-search-list'}>
           {}
           {filterMovies()?.map((movie) => (
-              <li><MovieCard key={movie.id} item={movie}/></li>
+              <li key={movie.id}><MovieCard item={movie}/></li>
           ))}
         </ul>
+        <ReactPaginate
+            breakLabel="..."
+            nextLabel="next"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={data?.total_pages}
+            previousLabel="previous"
+            renderOnZeroPageCount={null}
+            containerClassName={'pagination'}
+            pageClassName={'page-item'}
+            pageLinkClassName={'page-link'}
+            previousClassName={'page-item'}
+            nextClassName={'page-item'}
+            previousLinkClassName={'page-link'}
+            nextLinkClassName={'page-link'}
+            activeClassName={'active'}
+            disabledClassName={'disabled'}
+        />
       </div>
   )
 }
