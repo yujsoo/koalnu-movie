cedc2c67216a69ff1c925e51481050d2ec2f2b43
유지수 <jsyu333@gmail.com>
2026-04-17 00:29:46 +0900

    fix: 페이지네이션 오류 수정

--- src/hooks/useSearchMovie.js
+++ src/hooks/useSearchMovie.js
@@ -1,14 +1,14 @@
 import {useQuery} from "@tanstack/react-query";
 import api from "../util/api.js";
 
-const fetchSearchMovie = ({keyword}) => {
-  return keyword ? api.get(`/search/movie?query=${keyword}`) : api.get(`/movie/popular`);
+const fetchSearchMovie = ({keyword,page}) => {
+  return keyword ? api.get(`/search/movie?query=${keyword}&page=${page}`) : api.get(`/movie/popular`);
 }
 
-export const useSearchMovie = ({keyword}) => {
+export const useSearchMovie = ({keyword,page}) => {
   return useQuery({
-    queryKey: ['movie-search', keyword],
-    queryFn: () => fetchSearchMovie({keyword}),
+    queryKey: ['movie-search', keyword,page],
+    queryFn: () => fetchSearchMovie({keyword,page}),
     select:(result) => result.data
   })
 }
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -2,21 +2,24 @@ import {useSearchMovie} from "../../hooks/useSearchMovie.js";
 import {useSearchParams} from "react-router-dom";
 import MovieCard from "../../common/MovieCard/MovieCard.jsx";
 import './MoviPage.style.css'
-import React from 'react';
+import React, {useState} from 'react';
 import ReactPaginate from 'react-paginate';
 import ReactPaginateImport from "react-paginate";
-
 
 const MoviePage = () => {
   const [searchParams] = useSearchParams();
   const searchKeyword = searchParams.get("keyword");
+  const [page, setPage] = useState(1);
+
   const ReactPaginate =
       ReactPaginateImport?.default ?? ReactPaginateImport;
+
 
   // 경로 두가지
   // nav바에서 클릭해서 온 경우
   // keyword를 입력해서 온 경우 -> Keyword와 관련된 영화를 보여줌
-  const {data, isLoading, isError, error} = useSearchMovie({ keyword: searchKeyword });
+  const {data, isLoading, isError, error} = useSearchMovie({ keyword: searchKeyword , page:page
+  });
   if(isLoading) {
     return <div>Loading...</div>;
   }
@@ -25,8 +28,8 @@ const MoviePage = () => {
     return <div>{error.message}</div>
   }
 
-  const handlePageClick = () => {
-
+  const handlePageClick = (event) => {
+    setPage(event.selected + 1)
   }
 
   if (!data.results || data.results.length === 0) {
@@ -47,6 +50,7 @@ const MoviePage = () => {
             onPageChange={handlePageClick}
             pageRangeDisplayed={5}
             pageCount={data?.total_pages}
+            forcePage={page - 1}
             previousLabel="previous"
             renderOnZeroPageCount={null}
             containerClassName={'pagination'}
