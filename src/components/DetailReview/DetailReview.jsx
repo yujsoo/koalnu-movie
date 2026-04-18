404922c88af2f210e359b7063c55963580b01c7e
유지수 <jsyu333@gmail.com>
2026-04-18 10:11:52 +0900

    fix: 4. 리뷰가 길때 접기, 더보기 기능 넣어주기

--- src/components/DetailReview/DetailReview.jsx
+++ src/components/DetailReview/DetailReview.jsx
@@ -1,11 +1,13 @@
 import {useMovieReviewsQuery} from "../../hooks/useMovieReviews.js";
 import {useParams} from "react-router-dom";
 import './DetailReview.style.css'
+import {useState} from "react";
 
 const DetailReview = () => {
   const { id } = useParams();
 
   const {data, isLoading, isError, error} = useMovieReviewsQuery({movieId: id});
+  const [openReviews, setOpenReviews] = useState({});
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -41,31 +43,62 @@ const DetailReview = () => {
     });
   };
 
+  const toggleReview = (id) => {
+    setOpenReviews(prev => ({
+      ...prev,
+      [id]: !prev[id]
+    }));
+  };
+
   return (
       <div className={'box reviews'}>
         <h4>Reviews</h4>
-        {data.results.length === 0 ? <div className={'no-review'}>등록된 리뷰가 없습니다.</div> : <div className={'review_list'}>
-          {data.results.map(review => (
-              <div key={review.id} className={'list-item'}>
-                <div className={'reviewers'}>
-                  {review.author_details?.avatar_path ? (
-                      <div className={'reviewer'}>
-                        <img
-                            className="review-user-img"
-                            src={getProfileImage(review.author_details.avatar_path)}
-                            alt="user"
-                        />
+
+        {data?.results?.length === 0 ? (
+            <div className={'no-review'}>
+              등록된 리뷰가 없습니다.
+            </div>
+            ) : (
+            <div className={'review_list'}>
+              {data?.results?.map(review => {
+                const isOpen = openReviews[review.id];
+                const content = review.content ?? "";
+
+                return (
+                    <div key={review.id} className={'list-item'}>
+                      <div className={'reviewers'}>
+                        {review.author_details?.avatar_path ? (
+                            <div className={'reviewer'}>
+                              <img
+                                  className="review-user-img"
+                                  src={getProfileImage(review.author_details.avatar_path)}
+                                  alt="user"
+                              />
+                            </div>
+                        ) : (
+                            <DefaultProfile name={review.author} />
+                        )}
+                        <p className={'author'}>{review.author}</p>
                       </div>
-                  ) : (
-                      <DefaultProfile name={review.author} />
-                  )}
-                  <p className={'author'}>{review.author}</p>
-                </div>
-                <p className={'content'}>{review.content}</p>
-                <span className={'date'}>{formatDate(review.created_at)}</span>
-              </div>
-          ))}
-        </div>}
+                      <p className={`content ${isOpen ? "open" : ""}`}>
+                        {content}
+                      </p>
+                      {content && (
+                          <button
+                              className="more-btn"
+                              onClick={() => toggleReview(review.id)}
+                          >
+                            {isOpen ? "접기" : "더보기"}
+                          </button>
+                      )}
+                      <span className={'date'}>
+                        {formatDate(review.created_at)}
+                      </span>
+                    </div>
+                );
+              })}
+            </div>
+        )}
       </div>
   )
 }
