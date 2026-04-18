67cf01779bd8284a9da11d5649daf0438b69b27e
유지수 <jsyu333@gmail.com>
2026-04-18 11:04:06 +0900

    fix: footer 추가

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.style.css';
+
+const Footer = () => {
+  return (
+      <footer className="footer">
+        <div className="footer-inner">
+            <p>© 2026 Movie App. All rights reserved.</p>
+        </div>
+      </footer>
+  );
+};
+
+export default Footer;
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/NavBar.jsx";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer.jsx";
 
 const AppLayout = () => {
   return (
       <div>
         <NavBar/>
         <Outlet />
+        <Footer/>
       </div>
   )
 }
