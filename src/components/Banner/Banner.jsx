f6f24217f7068c0396bca8444ebce3b1dc54dc7c
유지수 <jsyu333@gmail.com>
2026-04-18 11:48:40 +0900

    fix: 홈으로 이동시 오류 수정

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 const Banner = () => {
   const navigate = useNavigate();
   const {data, isLoading, isError, error } = usePopularMoviesQuery();
-  console.log(data);
 
   if (isLoading) {
     return <h1>Loading...</h1>
@@ -16,16 +15,16 @@ const Banner = () => {
   }
   return (
       <div style={{
-     backgroundImage: "url(" + `https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces${data?.results[0].poster_path}` + ")",
+     backgroundImage: "url(" + `https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces${data?.results?.[0].poster_path}` + ")",
       }}
       className={'banner'}
       >
         <div className={'info'}>
-          <strong>{data?.results[0].title}</strong>
-          <p>{data?.results[0].overview}</p>
+          <strong>{data?.results?.[0].title}</strong>
+          <p>{data?.results?.[0].overview}</p>
           <div className={'ctrl-btn'}>
             <button type={'button'} className={'btn play-now'}>Play Now</button>
-            <button type={'button'} className={'btn'} onClick={() => navigate(`/movies/${data?.results[0].id}`)}>Details</button>
+            <button type={'button'} className={'btn'} onClick={() => navigate(`/movies/${data?.results?.[0].id}`)}>Details</button>
           </div>
         </div>
       </div>
